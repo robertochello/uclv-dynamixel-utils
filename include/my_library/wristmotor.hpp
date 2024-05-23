8a99ac3a75d304c10f13b32dff07782f9ee6b2c2
Roberto Chello <36032212+robertochello@users.noreply.github.com>
2024-05-23 20:59:42 +0200

    Add comments for wristmotor.hpp

--- include/my_library/wristmotor.hpp
+++ include/my_library/wristmotor.hpp
@@ -1,25 +1,45 @@
 #ifndef WRISTMOTOR_H
 #define WRISTMOTOR_H
 
-#include "dynamixel_sdk/dynamixel_sdk.h"
 #include "motor.hpp"
 
-
+/**
+ * @brief The WristMotor class represents a wrist motor, which is a specific type of motor.
+ */
 class WristMotor : public Motor {
+private:
+public:
 
-    private:
-    public:
-
-
-        WristMotor(const std::string& serial_port, int baudrate, float protocol_version, 
+    /**
+     * @brief Constructs a WristMotor object with specified parameters.
+     * 
+     * @param serial_port The serial port to which the motor is connected.
+     * @param baudrate The baudrate for the serial communication.
+     * @param protocol_version The protocol version used for communication.
+     * @param portHandler Pointer to the port handler.
+     * @param packetHandler Pointer to the packet handler.
+     * @param id Identifier of the motor.
+     */
+    WristMotor(const std::string& serial_port, int baudrate, float protocol_version, 
           dynamixel::PortHandler *const& portHandler, dynamixel::PacketHandler *const& packetHandler,
           uint8_t id);
 
-        WristMotor(const std::string& serial_port, int baudrate, float protocol_version,
+    /**
+     * @brief Constructs a WristMotor object with specified parameters.
+     * 
+     * @param serial_port The serial port to which the motor is connected.
+     * @param baudrate The baudrate for the serial communication.
+     * @param protocol_version The protocol version used for communication.
+     * @param id Identifier of the motor.
+     */
+    WristMotor(const std::string& serial_port, int baudrate, float protocol_version,
           uint8_t id);
 
-        WristMotor();
+    /**
+     * @brief Default constructor.
+     */
+    WristMotor();
 
 };
 
-#endif
+#endif // WRISTMOTOR_H
