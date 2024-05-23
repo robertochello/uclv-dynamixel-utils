6ee650e2dd5127cb5844f124ccf3bea73d5a6a72
Roberto Chello <36032212+robertochello@users.noreply.github.com>
2024-05-23 20:57:22 +0200

    Add comments for fingermotor.hpp

--- include/my_library/fingermotor.hpp
+++ include/my_library/fingermotor.hpp
@@ -1,26 +1,45 @@
 #ifndef FINGERMOTOR_H
 #define FINGERMOTOR_H
 
-#include "dynamixel_sdk/dynamixel_sdk.h"
 #include "motor.hpp"
 
-
+/**
+ * @brief The FingerMotor class represents a finger motor, which is a specific type of motor.
+ */
 class FingerMotor : public Motor {
+private:
+public:
 
-    private:
-    public:
-
-
-        
-        FingerMotor(const std::string& serial_port, int baudrate, float protocol_version, 
+    /**
+     * @brief Constructs a FingerMotor object with specified parameters.
+     * 
+     * @param serial_port The serial port to which the motor is connected.
+     * @param baudrate The baudrate for the serial communication.
+     * @param protocol_version The protocol version used for communication.
+     * @param portHandler Pointer to the port handler.
+     * @param packetHandler Pointer to the packet handler.
+     * @param id Identifier of the motor.
+     */
+    FingerMotor(const std::string& serial_port, int baudrate, float protocol_version, 
           dynamixel::PortHandler *const& portHandler, dynamixel::PacketHandler *const& packetHandler,
           uint8_t id);
 
-        FingerMotor(const std::string& serial_port, int baudrate, float protocol_version,
+    /**
+     * @brief Constructs a FingerMotor object with specified parameters.
+     * 
+     * @param serial_port The serial port to which the motor is connected.
+     * @param baudrate The baudrate for the serial communication.
+     * @param protocol_version The protocol version used for communication.
+     * @param id Identifier of the motor.
+     */
+    FingerMotor(const std::string& serial_port, int baudrate, float protocol_version,
           uint8_t id);
 
-        FingerMotor();
+    /**
+     * @brief Default constructor.
+     */
+    FingerMotor();
 
 };
 
-#endif
+#endif // FINGERMOTOR_H
