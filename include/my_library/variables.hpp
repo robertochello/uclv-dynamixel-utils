0741ecd83319eb0f1b2f59e88e8004739595d384
Roberto Chello <36032212+robertochello@users.noreply.github.com>
2024-05-23 20:58:54 +0200

    Add comments for variables.hpp

--- include/my_library/variables.hpp
+++ include/my_library/variables.hpp
@@ -1,36 +1,43 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
+// Serial device name
+#define DEVICENAME                "/dev/ttyUSB0"
 
-#define DEVICENAME                "/dev/ttyUSB0"     // Nome del dispositivo 
-#define BAUDRATE                  1000000            // Baudrate
-#define PROTOCOL_VERSION          2.0                // Protocol version
+// Serial communication baudrate
+#define BAUDRATE                  1000000
 
-#define MAIN_BOARD                30                 // Main board
-#define WIRST_ROTATION            31                 // Wirst rotation
-#define WIRST_ADDUCTION           32                 // Wirst adduction
-#define WIRST_FLEXION             33                 // Wirst flexion
-#define THUMB_ADDUCTION           34                 // Thumb adduction
-#define THUMB_FLEXION             35                 // Thumb flexion
-#define INDEX_FLEXION             36                 // Index flexion
-#define MIDDLE_FLEXION            37                 // Middle flexion
-#define LAST_FLEXION              38                 // 4th and 5th flexion
+// Protocol version
+#define PROTOCOL_VERSION          2.0
 
-#define ADDR_TORQUE_ENABLE        24                 // Torque enable
-#define ADDR_TARGET_POSITION      30                 // Target position
-#define ADDR_TARGET_SPEED         32                 // Target speed
-#define ADDR_PRESENT_POSITION     36                 // Present position
-#define ADDR_PRESENT_SPEED        38                 // Present speed
-#define ADDR_PRESENT_TEMPERATURE  43                 // Present temperature
+// Identifiers of various motors
+#define MAIN_BOARD                30
+#define WIRST_ROTATION            31
+#define WIRST_ADDUCTION           32
+#define WIRST_FLEXION             33
+#define THUMB_ADDUCTION           34
+#define THUMB_FLEXION             35
+#define INDEX_FLEXION             36
+#define MIDDLE_FLEXION            37
+#define LAST_FLEXION              38
 
-#define TORQUE_ENABLE             1                  // Torque enable value
-#define TORQUE_DISABLE            0                  // Torque disable value
+// Motor register addresses
+#define ADDR_TORQUE_ENABLE        24
+#define ADDR_TARGET_POSITION      30
+#define ADDR_TARGET_SPEED         32
+#define ADDR_PRESENT_POSITION     36
+#define ADDR_PRESENT_SPEED        38
+#define ADDR_PRESENT_TEMPERATURE  43
 
-#define LEN_PRESENT_POSITION      2                  // data length for ADDR_PRESENT_POSITION
-#define LEN_TARGET_POSITION       2                  // data length for ADDR_TARGET_POSITION
-#define LEN_PRESENT_SPEED         2                  // data length for ADDR_PRESENT_SPEED
-#define LEN_TARGET_SPEED          2                  // data length for ADDR_TARGET_SPEED
-#define LEN_PRESENT_TEMPERATURE   1                  // data length for ADDR_PRESENT_TEMPERATURE
+// Values to enable/disable motor control
+#define TORQUE_ENABLE             1
+#define TORQUE_DISABLE            0
 
+// Data lengths for motor registers
+#define LEN_PRESENT_POSITION      2
+#define LEN_TARGET_POSITION       2
+#define LEN_PRESENT_SPEED         2
+#define LEN_TARGET_SPEED          2
+#define LEN_PRESENT_TEMPERATURE   1
 
-#endif
+#endif // DEFINES_H
